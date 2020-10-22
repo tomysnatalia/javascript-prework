c222ae5e17d4dd4691e7209c7276030ef68af8bb
Natalia Tomys <hanf.natalia@gmail.com>
2020-10-22 13:43:17 +0200

    playerMove

--- js/script.js
+++ js/script.js
@@ -21,3 +21,15 @@ if (randomNumber == '1') {
 printMessage('Mój ruch: ' + computerMove);
 
 
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+	playerMove = 'papier';
+} else if (playerInput= '3') {
+	playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
