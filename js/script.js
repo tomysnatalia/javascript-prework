c64c6a51d85f5e2cfed7ee5f341df33ee11fa4ac
Natalia Tomys <hanf.natalia@gmail.com>
2020-10-22 13:41:00 +0200

    computerMove

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+var computerMove;
+
+var playerMove;
+
+var playerMove, playerInput;
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+	computerMove = 'papier';
+} else if (randomNumber == '3') {
+	computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
